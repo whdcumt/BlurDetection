d7ad25b80c5c3b94d9ed2ea8d0a84dbf58a93707
WillBrennan <william.brennan@kcl.ac.uk>
2015-03-24 11:55:59 +0000

    Added FocusMask

---
+++ FocusMask.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Will Brennan'
+
+# built-in modules
+import logging
+# Standard modules
+# Custom modules
+
+logger = logging.getLogger('main')
