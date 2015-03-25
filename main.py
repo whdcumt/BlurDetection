2fade99598f6b9add6ae9ea03e8ef6f0160a7175
WillBrennan <william.brennan@kcl.ac.uk>
2015-03-25 21:46:03 +0000

    Fixup in main processing script, wasn't entering states correctly

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ if __name__ == '__main__':
             logger.debug('evaluating {0}'.format(img_path))
             img = cv2.imread(img_path)
             if isinstance(img, numpy.ndarray):
-                scripts.display('dialog (blurry: Y?)', img)
-                blurry = False
-                if cv2.waitKey(0) in map(lambda i: ord(i), ['Y', 'y']):
-                    blurry = True
                 if args.testing:
+                    scripts.display('dialog (blurry: Y?)', img)
+                    blurry = False
+                    if cv2.waitKey(0) in map(lambda i: ord(i), ['Y', 'y']):
+                        blurry = True
                     x_axis = [1, 3, 5, 7, 9]
                     for x in x_axis:
                         img_mod = cv2.GaussianBlur(img, (x, x), 0)
@@ -71,7 +71,9 @@ if __name__ == '__main__':
                 else:
                     result, val = evaluate(img, args=args)
                     logger.info('fft average of {0}'.format(result))
-    if args.display:
+                    if args.display:
+                        scripts.display('blur_msk', result)
+    if args.display and args.testing:
         import matplotlib.pyplot as plt
         plt.scatter(x_okay, y_okay, color='g')
         plt.scatter(x_blur, y_blur, color='r')
