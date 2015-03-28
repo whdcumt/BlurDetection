cf927ba4b0f65054683d3a894fc35704e011da28
WillBrennan <william.brennan@kcl.ac.uk>
2015-03-28 10:27:14 +0000

    Resolved sizing bug with color and greyscale display

--- scripts.py
+++ scripts.py
@@ -93,10 +93,10 @@ def xfind_images(directory, recursive=False, ignore=True):
             yield path_a
 
 
-def display(title, img, max_size=500000):
+def display(title, img, max_size=200000):
     assert isinstance(img, numpy.ndarray), 'img must be a numpy array'
     assert isinstance(title, str), 'title must be a string'
-    scale = numpy.sqrt(min(1.0, float(max_size)/img.size))
+    scale = numpy.sqrt(min(1.0, float(max_size)/(img.shape[0]*img.shape[1])))
     logger.debug('image is being scaled by a factor of {0}'.format(scale))
     shape = (int(scale*img.shape[1]), int(scale*img.shape[0]))
     img = cv2.resize(img, shape)
