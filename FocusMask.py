fb5904d46aebfd90e8202ce5b5ba893c40abc251
WillBrennan <william.brennan@kcl.ac.uk>
2015-03-28 10:26:42 +0000

    Updated FocusMask to include new generation method

--- FocusMask.py
+++ FocusMask.py
@@ -31,7 +31,7 @@ def get_masks(img, n_seg=250):
     return masks[1:]
 
 
-def blur_mask(img):
+def blur_mask_old(img):
     assert isinstance(img, numpy.ndarray), 'img_col must be a numpy array'
     assert img.ndim == 3, 'img_col must be a color image ({0} dimensions currently)'.format(img.ndim)
     blur_mask = numpy.zeros(img.shape[:2], dtype=numpy.uint8)
@@ -45,6 +45,46 @@ def blur_mask(img):
     result = numpy.sum(blur_mask)/(255.0*blur_mask.size)
     logger.info('{0}% of input image is blurry'.format(int(100*result)))
     return blur_mask, result
+
+
+def closing(msk):
+    assert isinstance(msk, numpy.ndarray), 'msk must be a numpy array'
+    assert msk.ndim == 2, 'msk must be a greyscale image'
+    kernel = cv2.getGaussianKernel(5, sigma=-1)
+    msk = cv2.erode(msk, kernel, iterations=1)
+    msk[msk < 128] = 0
+    msk[msk > 127] = 255
+    return msk
+
+
+def remove_border(msk, width=50):
+    assert isinstance(msk, numpy.ndarray), 'msk must be a numpy array'
+    assert msk.ndim == 2, 'msk must be a greyscale image'
+    dh, dw = map(lambda i: i//width, msk.shape)
+    h, w = msk.shape
+    msk[:dh, :] = 255
+    msk[h-dh:, :] = 255
+    msk[:, :dw] = 255
+    msk[:, w-dw:] = 255
+    return msk
+
+def blur_mask(img):
+    assert isinstance(img, numpy.ndarray), 'img_col must be a numpy array'
+    assert img.ndim == 3, 'img_col must be a color image ({0} dimensions currently)'.format(img.ndim)
+    msk, val, blurry = main.blur_detector(img)
+    logger.debug('inverting img_fft')
+    msk = cv2.convertScaleAbs(255-(255*msk/numpy.max(msk)))
+    print numpy.max(msk), numpy.min(msk)
+    msk[msk < 50] = 0
+    msk[msk > 127] = 255
+    logger.debug('removing border')
+    msk = remove_border(msk)
+    logger.debug('applying erosion and dilation operators')
+    msk = closing(msk)
+    logger.debug('evaluation complete')
+    result = numpy.sum(msk)/(255.0*msk.size)
+    logger.info('{0}% of input image is blurry'.format(int(100*result)))
+    return msk, result
 
 
 if __name__ == '__main__':
